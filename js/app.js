bd57300e6334e1e88ab3df82f2e83f0397a38538
Jose Manuel <jose@261004@gmail.com>
2021-09-23 15:36:39 -0400

    Advancing with the proyect

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const $main = document.getElementById('main');
+const $gamesContainer = document.getElementById('gamesContainer');
+const $mainList = document.getElementById('mainList');
+let existingCategories = [];
 
 /*create new Element, append them and return them*/
 const newNode = ({ HTMLTag = 'div', parent, txtContent = '', classes = [] }) => {
@@ -16,7 +18,7 @@ const newNode = ({ HTMLTag = 'div', parent, txtContent = '', classes = [] }) => 
     return $node;
 }
 
-const getData = async () => {
+const getData$ = async () => {
     try {
         renderData(await (await fetch('games.json')).json());
     } catch (error) {
@@ -27,10 +29,66 @@ const getData = async () => {
 
 const renderData = (games) => {
     const fragment = document.createDocumentFragment();
+    const categoriesfragment = document.createDocumentFragment();
+    console.log(games);
     games.forEach((game) => {
-        const card = game.newNode({ parent: fragment });
 
+        const $card = newNode({
+            parent: fragment,
+            classes: ['card']
+        });
+
+        $card.game = game;
+
+        const $img = newNode({
+            HTMLTag: 'img',
+            parent: $card,
+            classes: ['card__img']
+        });
+
+        $img.setAttribute('alt', game.name);
+        $img.setAttribute('src', game.imageURL);
+
+        newNode({
+            HTMLTag: "button",
+            txtContent: "Buy",
+            parent: $card,
+            classes: ['card__button']
+        });
+
+        /* Show up categories */
+        game.categories.forEach((category) => {
+            if (!existingCategories.includes(category)) {
+                existingCategories = existingCategories.concat(category);
+                newNode({
+                    HTMLTag: 'li',
+                    parent: categoriesfragment,
+                    txtContent: category,
+                    classes: ['main__item']
+                });
+            }
+        });
     });
+    $gamesContainer.append(fragment);
+    $mainList.append(categoriesfragment);
 }
 
-getData()
+getData$();
+
+document.body.addEventListener('click', (e) => {
+    const $E = e.target;
+    if ($E.classList.contains('main__item')) {
+        document.querySelectorAll('.card--hidden').forEach((card) => card.classList.remove('card--hidden'));
+
+        if ($E.textContent === 'All')
+            return false;
+
+        document.querySelectorAll('.card').forEach((card) => {
+            const category = $E.textContent.trim();
+
+            if (!card.game.categories.includes(category))
+                card.classList.add('card--hidden');
+
+        });
+    }
+});
